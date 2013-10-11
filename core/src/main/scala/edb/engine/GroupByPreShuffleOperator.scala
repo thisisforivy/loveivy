93ba5399d40274c0d8f24f41176d3c0d4bb420c3
thisisforivy <thisisforivy@gmail.com>
2013-10-11 01:33:43 -0700

    Add gby preshuffle operator

---
+++ core/src/main/scala/edb/engine/GroupByPreShuffleOperator.scala
@@ -0,0 +1,91 @@
+package edb.engine
+
+import edb.parser._
+import edb.catalog._
+import spark.SparkContext
+import SparkContext._
+import spark.{RDD}
+import spark.{Logging}
+import scala.reflect.BeanProperty
+import scala.collection.mutable.ArrayBuffer
+
+class GroupByPreShuffleOperator(
+  inSchema: Schema, 
+  outSchema: Schema, 
+  gbyExpList: List[EXPRESSION],
+  aggExpList: List[EXPRESSION]) 
+extends UnaryOperator[SequenceRecord] {
+
+  private var aggInnerExpList: List[EXPRESSION]= _
+
+  //setup operator meta data
+  override def initializeOnMaster() {
+    assert(!Option(aggExpList).isEmpty)
+    //when group by clause is present
+    //gbyExpList is non-empty
+    //for select count(*) we do not have
+    //gby clause
+    assert(!Option(gbyExpList).isEmpty)
+
+    //create col or '*' expression from agg(ident)
+    def innerExp (exp: EXPRESSION): EXPRESSION = {
+      exp match {
+        case a1: SUM_EXP => a1.e
+        case a2: AVG_EXP => a2.e
+        case a3: MIN_EXP => a3.e
+        case a4: MAX_EXP => a4.e
+        case a5: CNT_EXP => a5.e
+        case _ =>throw new 
+        EdbException("not supported agg func ")
+      }
+    }
+
+    //extract the inner exp of agg func
+    aggInnerExpList= aggExpList.map(x=> innerExp(x))
+
+    //the schema of input record
+    setInSch(inSchema)
+    setOutSch(outSchema)
+  }
+
+  /** 
+    * This function convert the expList on 
+    * a given tuple to a <k,v> pair
+    *
+    */
+  def evalFunc (tuple: Any): 
+  (SequenceRecord, 
+    SequenceRecord) = {
+
+    //create (groupbyExpr, aggInnerExp)
+      (new SequenceRecord(
+      //for each expression, evaluate it
+        gbyExpList.map(
+          x => 
+          Utils.eval(x, 
+            tuple.asInstanceOf[SequenceRecord],
+            inSchema)).toArray 
+        ),
+      new SequenceRecord(
+      //for each expression, evaluate it
+        aggInnerExpList.map(
+          x => 
+          Utils.eval(x, 
+            tuple.asInstanceOf[SequenceRecord],
+            inSchema)).toArray 
+        ))
+    }
+
+  /**
+    * Reducer side aggregation
+    */
+  override def processPartition(
+    split: Int, 
+    iter: Iterator[_]) =  iter.map(row => evalFunc(row))
+
+  override def toString(): String = 
+     "GBYPRESHUFFLE--gby expression list: " + 
+     gbyExpList + ", agg expression list: " + 
+     aggExpList
+
+}
